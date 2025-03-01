676524b654a1c2f26d9194498cc63cdf180bbd6e
delbyte <blazed0utcode@gmail.com>
2025-03-01 11:04:39 +0530

    removed comment

--- notifier.py
+++ notifier.py
@@ -7,7 +7,7 @@ def notify_user(message, title="Browser Automation"): # Sends a notif to the use
             title=title,
             message=message,
             app_name="Web Accessibility App",
-            timeout=3  # Notification stays visible for 5 seconds
+            timeout=3 
         )
         logging.info("Notification sent successfully.")
     except Exception as e:
